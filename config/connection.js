4f48ed4e624b702c00bfb2b2c28e47c80e128a4b
mc4tech <chavezmeguel@yahoo.com>
2017-08-14 22:38:17 -0400

    JAWSDB_URL code

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 var mysql = require("mysql");
+var connection;
+if(process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+	}else{
+	var connection = mysql.createConnection({
+		host: "localhost",
+		user: "root",
+		password: "letmein",
+		database: "burgers_db"
+	});
 
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "letmein",
-	database: "burgers_db"
-});
-
-connection.connect(function(err) {
-	if(err) throw err;
-	console.log("connected as id : " + connection.threadid);
-});
+	connection.connect(function(err) {
+		if(err) throw err;
+		console.log("connected as id : " + connection.threadid);
+	});
+}
 
 module.exports = connection;
