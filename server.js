b0e044052c6e8b3af546d806fe0f94575527832c
mc4tech <chavezmeguel@yahoo.com>
2017-08-18 03:52:20 -0400

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-var path = require("path");
+// var path = require("path");
 //set handlebars
 var exphbs = require("express-handlebars");
 
@@ -13,7 +13,8 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use('/public', express.static(path.join(__dirname, 'app/public')));
+// app.use('/public', express.static(path.join(__dirname, 'app/public')));
+app.use(express.static(__dirname + "/public"));
 
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -25,7 +26,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 //import routes and give the server access to them
-var routes = require("./controllers/burgersController.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 app.use("/update", routes);
